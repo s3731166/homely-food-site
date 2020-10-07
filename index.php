ad02a7aac08b6184e8d76a27b25280313d8d0c83
s3773936 <71245601+s3773936@users.noreply.github.com>
2020-10-07 12:32:28 +1100

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,20 @@
     </div>
 </body>
 <footer>
-    Homely Food
+
+    <div class="row">
+        <div class="column">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="img/logo.png" alt="" style="height: 250px; width: 250px;"></div>
+            
+      <div class="column">
+            <h4 style="font-weight: bold; font-size:25px; text-align: center;" >CONTACT US</h4>
+            <br>
+            <img src="img/phone_footer.png" alt="" style="height: 30px; width: 30px;" > &nbsp;&nbsp;&nbsp;&nbsp; 93327349&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            
+           <img src="img/location_footer.png" alt="" style="height: 30px; width: 30px;">&nbsp;&nbsp;&nbsp;&nbsp;123 ABC Street&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            
+            <img src="img/email_footer1.png" alt="" style="height: 30px; width: 30px;">&nbsp;&nbsp;&nbsp;&nbsp;admin@homelyfood.com.au
+      </div>  
+    </div>
 </footer>
 
 </html>
