053daacac60fb7febfa882dde0127bc55313a3f3
Jessiah <s3731166@student.rmit.edu.au>
2020-10-07 13:55:26 +1100

    Footer uodates and styling

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 </body>
 <footer>
     <div class="row">
-        <div class="column">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="img/logo.png" alt="" style="height: 250px; width: 250px;"></div>
+        <div class="column">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://localhost/homely-food-site/img/logo.png" alt="" style="height: 250px; width: 250px;"></div>
             
-      <div class="column">
+      <div class="column" >
             <h4 style="font-weight: bold; font-size:25px; text-align: center;" >CONTACT US</h4>
             <br>
-            <img src="img/phone_footer.png" alt="" style="height: 30px; width: 30px;" > &nbsp;&nbsp;&nbsp;&nbsp; 93327349&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <img src="http://localhost/homely-food-site/img/phone_footer.png" alt="" style="height: 30px; width: 30px;" > &nbsp;&nbsp;&nbsp;&nbsp; 93327349&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             
-           <img src="img/location_footer.png" alt="" style="height: 30px; width: 30px;">&nbsp;&nbsp;&nbsp;&nbsp;123 ABC Street&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <img src="http://localhost/homely-food-site/img/location_footer.png" alt="" style="height: 30px; width: 30px;">&nbsp;&nbsp;&nbsp;&nbsp;123 ABC Street&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             
-            <img src="img/email_footer1.png" alt="" style="height: 30px; width: 30px;">&nbsp;&nbsp;&nbsp;&nbsp;admin@homelyfood.com.au
+            <img src="http://localhost/homely-food-site/img/email_footer1.png" alt="" style="height: 30px; width: 30px;">&nbsp;&nbsp;&nbsp;&nbsp;admin@homelyfood.com.au
       </div>  
     </div>
 </footer>
